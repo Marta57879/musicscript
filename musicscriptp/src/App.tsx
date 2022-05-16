76cbba62a5f28f98cff661db8db8f6a05052fce6
marta57879 <martastachyra94@gmail.com>
2022-05-16 10:56:05 +0200

    add bawol

--- musicscriptp/src/App.tsx
+++ musicscriptp/src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import bawol from './assets/bawol.webp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={bawol} className="pizmo" alt="bawol">
+      
+      </img>
+
+      <div className="topWrapper">
+
+        <div className="leftBox">
+        </div>
+        <div className="rightBox">
+        </div>
+
+       </div> 
+
+      <div className="firstBottomBox">
+
+      </div>
     </div>
   );
 }
